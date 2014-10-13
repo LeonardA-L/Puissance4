fc05916f4219423c45030904394715bcd016ae65
Anton Erholt <anton.erholt@gmail.com>
2014-10-14 00:34:03 +0200

    Moved websocket server code into server.pl Added some styling for the board and the page in general Added functionality for making a piece red or yellow Exported functions     setRedAt(row, col),     setYellowAt(row, col) and     clearBoard() to window, so now they're testable in the console

--- tests/webserver/js/main.js
+++ tests/webserver/js/main.js
@@ -1,11 +1,97 @@
-(function(window, document){
+(function(window, document) {
 
+    /* Semi-global variables */
+    var ws, board, boardElement;
+
+    var BOARD_WIDTH = 7;
+    var BOARD_HEIGHT = 6;
+
+    var EMPTY_SYMBOL = '_';
+    var RED_SYMBOL = 'r';
+    var YELLOW_SYMBOL = 'y';
+
+
+    var clearBoard = function() {
+        createBoard();
+        boardToTable();
+    };
+
+    var createBoard = function() {
+        board = [];
+        for (var i = 0; i < BOARD_HEIGHT; i++) {
+            var row = [];
+            for (var j = 0; j < BOARD_WIDTH; j++) {
+                row.push(EMPTY_SYMBOL);
+            }
+            board.push(row);
+        }
+    };
+
+    var setCellClassesTo = function(row, column, classes) {
+        var elem = boardElement.querySelectorAll('tr')[row].querySelectorAll('td')[column];
+        elem.setAttribute('class', classes);
+    };
+
+    var setRedAt = function(row, column) {
+        setCellClassesTo(row, column, 'cell red');
+    };
+
+    var setYellowAt = function(row, column) {
+        setCellClassesTo(row, column, 'cell yellow');
+    };
+
+    var setEmptyAt = function(row, column) {
+        setCellClassesTo(row, column, 'cell');
+    };
+
+    var boardToTable = function() {
+        if (!board) {
+            board = createBoard();
+        }
+
+        for (var row = 0; row < BOARD_HEIGHT; row++) {
+            for (var col = 0; col < BOARD_WIDTH; col++) {
+                var sym = board[row][col];
+                if (RED_SYMBOL == sym) {
+                    setRedAt(row, col);
+                } else if (YELLOW_SYMBOL == sym) {
+                    setYellowAt(row, col);
+                } else {
+                    setEmptyAt(row, col);
+                }
+            }
+        }
+    };
+
+    var addCanvasElement = function() {
+        var canvas = document.createElement('canvas');
+        ctx = canvas.getContext('2d');
+        canvas.id = 'canv1';
+        document.body.appendChild(canvas);
+    };
+
+    var removeLoadingMessage = function() {
+        var p = document.querySelector('#loading');
+        p.remove();
+    };
+
+    var setUpWebSocket = function() {
+        ws = new WebSocket('ws://127.0.0.1:9000/ws');
+
+        ws.onopen = function(event) {
+            console.log('WebSocket connection up and running!');
+            removeLoadingMessage();
+        }
+    };
 
     var main = function() {
-        var c;
-        c = document.querySelector('#canv');
-        console.log(c);
+        setUpWebSocket();
+        boardElement = document.querySelector('#board');
+        createBoard();
     }
+    window.setRedAt = setRedAt;
+    window.setYellowAt = setYellowAt;
+    window.clearBoard = clearBoard;
+    window.onload = main;
 
-   window.onload = main;
 })(this, this.document)
--- tests/webserver/server.pl
+++ tests/webserver/server.pl
@@ -1,16 +1,34 @@
-:- use_module(library(http/thread_httpd)).
+:- use_module(library(su)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/websocket)).
 :- use_module(library(http/http_server_files)).
 
+
+%% Paths/routes
 :- http_handler(root(.),	http_reply_file('index.html', []), []).
 :- http_handler(root(move),	move, []).
 :- http_handler(css('main.css'),  http_reply_file('css/main.css', []), []).
 :- http_handler(js('main.js'), http_reply_file('js/main.js', []), []).
+:- http_handler(root(ws), http_upgrade_to_websocket(websocket_handler, []), [spawn([])]).
 
 
 http:location(css, root(css), []).
 http:location(js, root(js), []).
 
+
+%% At the moment a simple echo
+websocket_handler(WebSocket) :-
+    ws_receive(WebSocket, Message),
+    (Message.opcode == close
+    ->  true
+    ;   ws_send(WebSocket, Message),
+     websocket_handler(WebSocket)
+    ).
+
+
+
+%% Sets up the server
 serve:-
     http_server(http_dispatch, [port(9000)]).
+
 :- serve.
