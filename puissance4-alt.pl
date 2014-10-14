f236100d014087179a9e280767a26e8fe53db090
julien-monniot <n.ceska@gmail.com>
2014-10-14 22:55:26 +0200

    C'est pas encore ça, mais on progresse....

--- puissance4-alt.pl
+++ puissance4-alt.pl
@@ -9,8 +9,8 @@
 
 % Test purpose : 
 
-isWin(_, _, _) :- random_between(0, 10, 1).
-isNotWin(_, _, _) :- \+isWin(_,_,_).
+isWin :- random_between(0,10,1).
+isNotWin :- \+isWin.
 
 %-----------------------
 
@@ -24,6 +24,7 @@ gagnant(pasDeGagnant).
 victory:-gagnant(joueurA), write('Joueur A gagne\n').
 victory:-gagnant(joueurB), write('Joueur B gagne\n').
 
+% check if the move the player wanna do is correct :
 moveIsOk(Pos) :-
 			Pos < 8,
 			Pos > 0,
@@ -31,46 +32,46 @@ moveIsOk(Pos) :-
 			findElem(L,Pos,Res),
 			Res < 1.
 
+% in case the move was not valid, replay.
 replay(1) :- aiplayA(1).
 replay(2) :- aiplayB(2).
 
-changeWinner(1):- assert(gagnant(joueurA)).
-changeWinner(2):- assert(gagnant(joueurB)).
+% change gagnant(pasDeGagnant) to set a winner
+changeWinner(1):- retract(gagnant(pasDeGagnant)), assert(gagnant(joueurA)).
+changeWinner(2):- retract(gagnant(pasDeGagnant)), assert(gagnant(joueurB)).
 
-% Check wether player can move the token in the column : if he can, the predicate is false, prolog will try the following one. If he can't, predicate is true, writes "bad move" and ask the user for another column.
+placeAToken(Pos,Player):- write('In 1st placeAToken\n'), 
+						  \+moveIsOk(Pos), 
+						  write('bad move\n'), 
+						  replay(Player),
+						  write('Out of 1st placeAToken\n').
 
-	placeAToken(Pos,Player):- write('In 1st placeAToken\n'), 
-							  \+moveIsOk(Pos), 
-							  write('bad move\n'), 
-							  replay(Player),
-							  write('Out of 1st placeAToken\n').   %should rarely come out
+placeAToken(Pos,Player):- write('In 2nd placeAToken\n'),
+						  write(Player),
+						  write('\n'), 
+						  write(Pos),
+						  write('\n'), 
+						  add(Pos,Player), 
+						  isNotWin,
+						  write('Out of 2nd placeAToken\n').				 
 
-% If last predicate returned false, try adding the token and check if the player won :
+placeAToken(_,Player) :-write('In 3rd placeAToken\n'), 
+						  changeWinner(Player),
+						  write('Out of 3rd placeAToken\n').
 
-	placeAToken(Pos,Player) :- write('In 2nd placeAToken\n'),
-							   write(Player),
-							   write('\n'), 
-							   write(Pos), 
-							   add(Pos,Player), 
-							   isNotWin(_, _, _),
-							   write('Out of 2nd placeAToken\n').
-
-% if the last one failed too, maybe the user has won, in that case, alert the rest of the soft
-
-	placeAToken(_,Player) :- write('In 3rd placeAToken\n'), 
-							 isWin(_, _, _), 
-							 retract(gagnant(pasDeGagnant)),
-							 changeWinner(Player),
-							 write('Out of 3rd placeAToken\n').
 
 % PlayerA makes a move, then you check if he won or not, and if he didn't, you keep going.
-%If gagnant(c) is false, it means playerA won : then victory will be true and loop will exit. 
-%Also in that case, playTurn will fail after aiplayA(1), which will not affect the last move or the predicate gagnant (because they use "assert")
-%If gagnant(c) is true, nothing has changed, A has not won, we keeo going with B. 
+% If gagnant(c) is false, it means playerA won : then victory will be true and loop will exit. 
+% Also in that case, playTurn will fail after aiplayA(1), which will not affect the 
+% last move or the predicate gagnant (because they use "assert")
+% If gagnant(c) is true, nothing has changed, A has not won, we keeo going with B. 
 % aiplayA(1) calls a predicate in ai1 which then calls to placeAToken here
 
-	playTurn :- aiplayA(1), gagnant(c), aiplayB(2).
+	playTurn :- aiplayA(1), aiplayB(2).
 
 % Start a game : loop on playTurn until "\+victory" is false.
 	
-	playGame :- repeat, playTurn,(\+victory).
+	%playGame :- repeat,(victory), playTurn.
+
+	playGame :- playTurn, victory.
+	playGame :- write('Game ended, player won').
